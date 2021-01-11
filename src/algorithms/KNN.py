f384fa12a1eb893c7e2d44e69c47ba4e536c2582
juantztz <tzintzun@ualberta.ca>
2021-01-11 11:02:10 -0600

    actaulizacion de hoy

--- src/algorithms/KNN.py
+++ src/algorithms/KNN.py
@@ -1,5 +1,4 @@
 from sklearn.model_selection import train_test_split,TimeSeriesSplit
-import time
 import numpy as np
 from math import sqrt
 from sklearn.metrics import mean_squared_error, mean_absolute_error
@@ -14,17 +13,14 @@ def KNN(X_train,X_test,y_train,y_test, metric, n_neighbors):
     # k-Nearest Neighbor
     print("k-Nearest Neighbor Regressor")
     # fit a k-nearest neighbor model to the data
-    startTime = time.time()
     model = KNeighborsRegressor(n_neighbors =14,metric=metric)
     print(model)
     model.fit(X_train, y_train)
-    running = time.time() - startTime
-    print("--- %s seconds ---" % (time.time() - startTime))
     # make predictions
     expected = y_test
     predicted = model.predict(X_test)
 
-    return expected, predicted, running
+    return expected, predicted
 
 
 def main(data, params):
@@ -43,10 +39,11 @@ def main(data, params):
     X_split = np.split(X_t,[index_split])
     Y_split = np.split(Y_t,[index_split])
 
-    X_t_1Y = X_split[0]
-    y_t_1Y = Y_split[0]
-    X_t_2Y = X_split[1]
-    y_t_2Y = Y_split[1]
+    X_t_train = X_split[0]
+    y_t_train = Y_split[0]
+    X_t_test = X_split[1]
+    y_t_test = Y_split[1]
+    print("X train", X_t_train.shape,"y train",y_t_train.shape,"X test",X_t_test.shape,"y test",y_t_test.shape)
 
     err_mae =[]
     err_mape = []
@@ -55,19 +52,14 @@ def main(data, params):
     pred = []
     tscv = TimeSeriesSplit(params["n_splits"])
 
-    for train_index, test_index in tscv.split(X_t):
+    for train_index, test_index in tscv.split(X_t_train):
         print("TRAIN:", train_index, "TEST:", test_index)
-        X_train, X_test = X_t_2Y[train_index],X_t_2Y[test_index]
-        X_train = np.asarray(X_train)
-        X_test = np.asarray(X_test)
-        y_train, y_test = y_t_2Y[train_index], y_t_2Y[test_index]
-        y_train=np.ravel(y_train)
-        y_test=np.ravel(y_test)
+        X_train, X_test = X_t_train[train_index], X_t_train[test_index]
+        y_train, y_test = y_t_train[train_index], y_t_train[test_index]
         print("X train",X_train.shape,"y train",y_train.shape,"X test",X_test.shape,"y test",y_test.shape)
+        expected ,predicted  = KNN(X_train,X_test,y_train,y_test, params["metric"], params["n_neighbors"])
 
-        expected ,predicted ,running = KNN(X_train,X_test,y_train,y_test, params["metric"], params["n_neighbors"])
         pred.append(predicted)
-        run.append(running)
         #Metricas
         MAE = mean_absolute_error(expected, predicted)
         err_mae.append(MAE)
